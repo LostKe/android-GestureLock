2557a42e7af9fa005b988420777a23b0807e5210
zsq <shu_qingzhang@foxmail.com>
2016-09-26 16:33:09 +0800

    up

--- app/src/main/java/zs/com/gesturelock/GestureEditActivity.java
+++ app/src/main/java/zs/com/gesturelock/GestureEditActivity.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import zs.com.gesturelock.widget.DotViewGroup;
+import zs.com.gesturelock.widget.GestureEditEnum;
 import zs.com.gesturelock.widget.GestureEnum;
 import zs.com.gesturelock.widget.GestureLockViewGroup;
 
@@ -21,7 +22,6 @@ import zs.com.gesturelock.widget.GestureLockViewGroup;
  */
 public class GestureEditActivity extends AppCompatActivity implements View.OnClickListener{
 
-    boolean hasEdit=true;
 
     List<Integer> answer=new ArrayList<Integer>();
 
@@ -33,6 +33,7 @@ public class GestureEditActivity extends AppCompatActivity implements View.OnCli
 
     TextView gesture_edit_tip;
 
+    GestureEditEnum editStatu=GestureEditEnum.EDIT_INIT;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture_edit);
@@ -68,11 +69,11 @@ public class GestureEditActivity extends AppCompatActivity implements View.OnCli
                 if(list==null || list.size()==0){
                     return;
                 }
-                if(hasEdit){//第一次编辑
+                if(editStatu==GestureEditEnum.EDIT_INIT){//第一次编辑
                     dotViewGroup.setPath(list);
-                    hasEdit=false;
                     answer.addAll(list);
                     resetTv.setVisibility(View.VISIBLE);
+                    editStatu=GestureEditEnum.EDIT_FIRST;
 
                 }else{
                     //比较和第一的手势
@@ -82,13 +83,11 @@ public class GestureEditActivity extends AppCompatActivity implements View.OnCli
                         //TODO
                     }else {
                         //提示不一致
-                        makeTip(false);
+                        editStatu=GestureEditEnum.EDIT_NOT_MATCH;
                     }
 
                 }
-
-
-
+                makeTip(editStatu);
             }
         });
 
@@ -114,22 +113,29 @@ public class GestureEditActivity extends AppCompatActivity implements View.OnCli
             case R.id.rest_tv:
                 resetTv.setVisibility(View.INVISIBLE);
                 answer.clear();
-                hasEdit=true;
+                editStatu=GestureEditEnum.EDIT_INIT;
                 dotViewGroup.reset();
-                makeTip(true);
+                makeTip(editStatu);
                 break;
         }
     }
 
-    private void makeTip(boolean b){
-        if(b){
-            gesture_edit_tip.setText("绘制解锁图案");
-            gesture_edit_tip.setTextColor(0xFF979999);
-        }else{
-            gesture_edit_tip.setText("两次绘制图案不一致，请重新绘制");
-            gesture_edit_tip.setTextColor(Color.RED);
-            Animation animation = AnimationUtils.loadAnimation(GestureEditActivity.this,R.anim.shake);
-            gesture_edit_tip.startAnimation(animation);
+    private void makeTip(GestureEditEnum value){
+        switch (value){
+            case EDIT_INIT:
+                gesture_edit_tip.setText("绘制解锁图案");
+                gesture_edit_tip.setTextColor(0xFF979999);
+                break;
+            case EDIT_FIRST:
+                gesture_edit_tip.setText("再次绘制解锁图案");
+                break;
+            case EDIT_NOT_MATCH:
+                gesture_edit_tip.setText("两次绘制图案不一致，请重新绘制");
+                gesture_edit_tip.setTextColor(Color.RED);
+                Animation animation = AnimationUtils.loadAnimation(GestureEditActivity.this,R.anim.shake);
+                gesture_edit_tip.startAnimation(animation);
+                break;
         }
+
     }
 }
---
+++ app/src/main/java/zs/com/gesturelock/widget/GestureEditEnum.java
@@ -0,0 +1,10 @@
+package zs.com.gesturelock.widget;
+
+/**
+ * Created by youx on 2016-09-26.
+ */
+public enum  GestureEditEnum {
+        EDIT_INIT,//初始状态
+        EDIT_FIRST,//第一次绘制
+        EDIT_NOT_MATCH;//两次绘制不一致
+}
